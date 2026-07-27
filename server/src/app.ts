4d8cf43180200e3a7bdff35dc1789f18fe442476
burhan5246 <burhanuddinsarangpurwala@gmail.com>
2026-07-27 10:58:27 +0530

    enabled cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-/* import cors from 'cors';
-import { env } from './config/env'; */
+import cors from 'cors';
+import { env } from './config/env';
 import routes from './routes';
 import { errorHandler } from './middleware/errorHandler';
 
@@ -9,13 +9,15 @@ const app = express();
 // --- Middleware ---
 
 // CORS — configured via environment variable (see §2.11)
-/* app.use(
+app.use(
   cors({
     origin: env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: false,
   })
-); */
+);
+
+console.log("CLIENT_URLCLIENT_URLCLIENT_URLCLIENT_URL", env.CLIENT_URL)
 
 // Body parsing — explicit limit prevents large payload attacks
 app.use(express.json({ limit: '10kb' }));
